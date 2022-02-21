2b5788e1507346ecad6aa961f863cc5d6907b73c
AlejandroOrozco456 <alejandrogobass@gmail.com>
2022-02-21 12:53:58 -0600

    Practica1 Web personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contenidoComponent } from './components/contenido/contenido.componet';
+import { HeaderComponent } from './components/header/header.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    contenidoComponent,
-    FooterComponent,
-    HomeComponent,
-    ContactoComponent
+    HeaderComponent,
+    ContenidoComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contenido/contenido.component.spec.ts
+++ src/app/components/contenido/contenido.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactoComponent } from './contacto.component';
+import { ContenidoComponent } from './contenido.component';
 
-describe('ContactoComponent', () => {
-  let component: ContactoComponent;
-  let fixture: ComponentFixture<ContactoComponent>;
+describe('ContenidoComponent', () => {
+  let component: ContenidoComponent;
+  let fixture: ComponentFixture<ContenidoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactoComponent ]
+      declarations: [ ContenidoComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContactoComponent);
+    fixture = TestBed.createComponent(ContenidoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
