1453b873a5cc453e4d6d62db0e5a2c2154806056
AlejandroOrozco456 <alejandrogobass@gmail.com>
2022-02-21 12:57:24 -0600

    Practica2 Agregar los links de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contenidoComponent } from './components/contenido/contenido.componet';
+import { HeaderComponent } from './components/header/header.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
+import { InformacionComponent } from './components/informacion/informacion.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    contenidoComponent,
+    HeaderComponent,
+    ContenidoComponent,
     FooterComponent,
     HomeComponent,
+    InformacionComponent,
     ContactoComponent
   ],
   imports: [
